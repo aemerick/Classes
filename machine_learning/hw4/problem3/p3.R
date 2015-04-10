38b3318521bb55f9f3344c7b661a887f04076b09
aemerick <aemerick11@gmail.com>
2015-04-10 19:23:43 -0400

    EM algorithm working and tested

--- machine_learning/hw4/problem3/p3.R
+++ machine_learning/hw4/problem3/p3.R
@@ -3,7 +3,10 @@
 # Course: STATW4400
 # HW    : HW 4
 #
-
+#  This file contains the clustering algorithm for problem 3
+#  of homework 4
+#
+#
 norm_vec <- function(v){
 
     if (dim(v)[1] > 1){
@@ -18,19 +21,13 @@ norm_vec <- function(v){
 MultinomialEM <- function(H, K,tau){
     # multinomial EM with histogram matrix H
     # number of classes  K, and threshold tau
-    n <- dim(H)[1]
-    print(dim(H))
+    n <- dim(H)[1] 
 
     # step 1: choose K at random and calculate centroids
     selection <- sample(1:K)
-    selection <- c(50,6000,10000)
-    H <- H + 0.01
-    H  <- H / rowSums(H)
-    
+    H <- H  + 0.01    
     tk <- H[selection,] 
-    tk <- tk / rowSums(tk)
-    
-    print('tk dim');    print(dim(tk))
+    tk <- tk / rowSums(tk) + 0.01
 
     # initialize phi and a 
     phi   <- matrix(rep(0,n*K),nrow=n,ncol=K)
@@ -40,32 +37,26 @@ MultinomialEM <- function(H, K,tau){
     # initialize ck to random values and normalize so
     # sum of ck = 1
     ck  <- runif(K,0,1)
-    ck  <- c(1,0.5,0.33333333)
     ck  <- ck / sum(ck)
 
     iter  <- 1
     keeplooping <- TRUE
     max_iter <- 500
     while (keeplooping){
-        prevP <- P
-        # Now do the E-step
-        for (kk in 1:K) {
-            phi[,kk] <- exp( H %*% (log(tk[kk,])))
-           
-            P[,kk]   <- ck[kk] * phi[,kk]
-        }
+        prevP <- P 
+ 
+        # E-step
+        phi <- exp(H %*% log(t(tk)))
+        P   <- ck * phi
+        P   <- P / rowSums(P)
 
-        # normalize a 
-        P <- P / rowSums(P)
 
-        # Now do the M step
+        # M-step
         ck   <- colSums(P)/n
-
         bk   <- t(P) %*% H
-
         tk   <- bk / rowSums(bk)
 
-        # A is the matrix ak
+        # Check for convergence
         delta <- norm( P - prevP,"O") #L1 matrix norm
         iter  <- iter + 1
 
@@ -73,24 +64,19 @@ MultinomialEM <- function(H, K,tau){
         if ((delta < tau) || (iter > max_iter)){
             keeplooping <- FALSE
         }
-        print(tk)
+        
     }
 
-    print(iter)
+    print(sprintf('Converged in %i iterations',iter))
+  
+    # convert soft assignments to hard assignments
     m <- c(rep(0,n))
     for (i in 1:n){
         m[i] <- which.max(P[i,])
     }
-    print(P[sample(1:30),])
+
     return(m)
 }
 
 
-# loads in the Histogram matrix
-# 40000 histograms, 16 bins per histogram
-H <- matrix(readBin("histograms.bin", "double", 640000), 40000, 16)
-
-# adjust for zero H's
-#H <- H + 0.001
-#H <- H[1:10,]
 
--- machine_learning/hw4/problem3/run_p3.R
+++ machine_learning/hw4/problem3/run_p3.R
@@ -1,8 +1,39 @@
+# Author: Andrew Emerick - aje2123@columbia.edu
+# UNI   : aje2123
+# Course: STATW4400
+# HW    : HW 4
+#
+#  This file contains the code used to run and plot the results
+#  from the clustering algorithm in problem 3 of homework 4
 #
 
-K = 3
-tau = 10.0
+source("p3.R")
 
+# choose a K and a tau
+K <- 2
+tau <- 1.0E-10
+
+# load in H
+H <- matrix(readBin("histograms.bin", "double", 640000), 40000, 16)
+
+# run the algorithm
 m <- MultinomialEM(H,K,tau)
-hist(m, breaks=c(0,1,2,3,4,5),freq=FALSE)
 
+n <- length(m)
+frac <- c(rep(0,K))
+
+for (k in 1:K){
+    print(noquote(sprintf("k = %.1f - %.3f",k,length(m[m==k])/n)))
+    frac[k] = length(m[m==k])/n
+}
+write(frac, noquote(sprintf("k%i.out",K)),ncolumns=1)
+matrix_m <- matrix(m,nrow=200,ncol=200)
+
+# function to rotate image matrix by 90 degrees
+rotate_function <- function(x) t(x)[,nrow(x):1]
+
+# plot and save
+pdf(noquote(sprintf("k%i.pdf",K)))
+image(rotate_function(t(matrix_m)),col=gray( seq(0,1,1/(K-1) )))
+dev.off()
+graphics.off()
